5db15e0fbc710f51707b6f82e31d91dd6981c210
unknown <mustafamjaber@yahoo.com>
2023-05-23 16:01:15 +0200

    finsh the trending component

--- find/app/components/Trending.jsx
+++ find/app/components/Trending.jsx
@@ -7,57 +7,81 @@ export default function Trending() {
       <h1 className="text-4xl capitalize ml-6 text-slate-900 dark:text-white">
         Trending destinations
       </h1>
-      <div className="flex  items-center justify-center m-auto mt-9 w-[100%] ">
-        <div className="w-1/2  flex  items-center justify-center">
+      <div className="flex  items-center justify-center gap-3 m-auto mt-9 w-[100%] px-8 ">
+        <div className="w-1/2 h-[280px] flex  items-center justify-center  relative rounded">
           <Image
-            src="https://media.timeout.com/images/105859738/image.jpg"
+            src="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/507000/507808-istanbul.jpg"
             alt="img"
-            width={500}
-            height={500}
-            className="rounded"
+            fill
+            className="rounded-lg "
+            style={{ objectFit: "cover" }}
           />
+          <div className="absolute top-4 left-4 text-white uppercase text-lg font-bold">
+            <h1>istanbul</h1>
+          </div>
         </div>
-        <div className="w-1/2  flex  items-center justify-center">
+        <div className="w-1/2 h-[280px] flex  items-center justify-center  relative rounded">
           {" "}
           <Image
-            src="https://media.timeout.com/images/105859738/image.jpg"
+            src="https://destinationwellknown.com/wp-content/uploads/2023/03/where-to-stay-mallorca.webp"
             alt="img"
-            width={500}
-            height={500}
-            className="rounded"
+            fill
+            className="rounded-lg  "
+            style={{ objectFit: "cover" }}
           />
+          <div className="absolute top-4 left-4 text-white uppercase text-lg font-bold">
+            <h1>majorca</h1>
+          </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 items-center w-[95%] m-auto mt-8">
-        <div className="">
+      <div className="grid grid-cols-3 gap-3 items-center w-[95%] m-auto mt-8">
+        <div className="relative w-[100%] h-[200px]">
           {" "}
           <Image
-            src="https://media.timeout.com/images/105859738/image.jpg"
+            src="https://americadomani.com/wp-content/uploads/2023/02/rome7th.jpg"
             alt="img"
-            width={500}
+            /*  width={500}
             height={500}
-            className="rounded"
+            className="rounded-lg " */
+            fill
+            className="rounded-lg  "
+            style={{ objectFit: "cover" }}
           />
+          <div className="absolute top-4 left-4 text-white uppercase text-lg font-bold">
+            <h1> rome</h1>
+          </div>
         </div>
-        <div className="">
+        <div className="relative w-[100%] h-[200px]">
           {" "}
           <Image
-            src="https://media.timeout.com/images/105859738/image.jpg"
+            src="https://cdn.destguides.com/files/store/itinerary/990/background_image/webp_large_202112291737-fa24970d2b2bf67cee7ec041d0dcb5b3.webp"
             alt="img"
-            width={500}
+            /* width={500}
             height={500}
-            className="rounded"
+            className="rounded-lg " */
+            fill
+            className="rounded-lg  "
+            style={{ objectFit: "cover" }}
           />
+          <div className="absolute top-4 left-4 text-white uppercase text-lg font-bold">
+            <h1>athens</h1>
+          </div>
         </div>
-        <div className="">
+        <div className="relative w-[100%] h-[200px]">
           {" "}
           <Image
-            src="https://media.timeout.com/images/105859738/image.jpg"
+            src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/01/a3/55.jpg"
             alt="img"
-            width={500}
-            height={500}
-            className="rounded"
+            /* width={500}
+            height={600}
+            className="rounded-lg " */
+            fill
+            className="rounded-lg  "
+            style={{ objectFit: "cover" }}
           />
+          <div className="absolute top-4 left-4 text-white uppercase text-lg font-bold">
+            <h1>cairo</h1>
+          </div>
         </div>
       </div>
     </section>
--- find/next.config.js
+++ find/next.config.js
@@ -12,6 +12,38 @@ const nextConfig = {
         port: "",
         pathname: "/images/105859738/image.jpg",
       },
+      {
+        protocol: "https",
+        hostname: "a.travel-assets.com",
+        port: "",
+        pathname:
+          "/findyours-php/viewfinder/images/res70/507000/507808-istanbul.jpg",
+      },
+      {
+        protocol: "https",
+        hostname: "destinationwellknown.com",
+        port: "",
+        pathname: "/wp-content/uploads/2023/03/where-to-stay-mallorca.webp",
+      },
+      {
+        protocol: "https",
+        hostname: "americadomani.com",
+        port: "",
+        pathname: "/wp-content/uploads/2023/02/rome7th.jpg",
+      },
+      {
+        protocol: "https",
+        hostname: "media.tacdn.com",
+        port: "",
+        pathname: "/media/attractions-splice-spp-674x446/0b/01/a3/55.jpg",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.destguides.com",
+        port: "",
+        pathname:
+          "/files/store/itinerary/990/background_image/webp_large_202112291737-fa24970d2b2bf67cee7ec041d0dcb5b3.webp",
+      },
     ],
   },
 };
