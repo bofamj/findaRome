4d32fe6a6d19191f61a3b4ceeed08bf3db16676e
unknown <mustafamjaber@yahoo.com>
2023-06-04 13:33:35 +0200

    install lodash and add the paginations component

---
+++ find/app/components/Paginations.jsx
@@ -0,0 +1,6 @@
+"use client";
+import { Pagination } from "@mui/material";
+
+export default function Paginations() {
+  return <Pagination count={10} variant="outlined" shape="rounded" />;
+}
--- find/app/hotel/page.jsx
+++ find/app/hotel/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Property from "../components/Property";
+import Paginations from "../components/Paginations";
 
 export default function Hotels() {
   return (
     <main className=" min-h-screen px-24 mt-20  text-slate-800  bg-gray-100 dark:bg-zinc-900">
       <Property />
+      <Paginations />
     </main>
   );
 }
