83bc1f28d91705b52cdf66bd3812d36225f2441e
unknown <mustafamjaber@yahoo.com>
2023-10-07 12:33:15 +0200

    fix the search bar

--- find/app/components/SerchBar.jsx
+++ find/app/components/SerchBar.jsx
@@ -12,16 +12,16 @@ const SerchBar = () => {
   const router = useRouter();
 
   return (
-    <div className="text-left text-sm  py-5 mx-4  flex  justify-center md:flex-row md:m-auto md:text-lg">
+    <div className="text-left text-sm  py-1 mx-1  flex  justify-center md:flex-row md:m-auto md:text-lg">
       <input
-        className="rounded  mr-3 p-2 w-[200px] dark:bg-slate-900 dark:text-white dark:placeholder:text-white md:w-[450px]"
+        className="rounded  mr-2 p-2 w-[270px] h-[40PX] dark:bg-slate-900 dark:text-white dark:placeholder:text-white md:w-[450px]"
         type="text"
         placeholder="State, city or town"
         value={location}
         onChange={(e) => setLocation(e.target.value)}
       />
       <button
-        className="rounded bg-blue-600 px-3 md:px-9 py-5 mr-2 mt-3 md:mt-0 text-white  dark:bg-red-900"
+        className="rounded bg-blue-600 px-1 md:px-9 py-0 md:py-2 max-h-10 mr-1 mt-0 md:mt-0 text-white  dark:bg-red-900"
         onClick={() => {
           if (location === "") return;
           router.push(`/serchLocation?city=${location}`);
